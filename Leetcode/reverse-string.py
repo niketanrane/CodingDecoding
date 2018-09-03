2adeaaafceb53d8224b7453c6093b366ee13c04c
Niketan Santosh Rane <40956529+niketanrane@users.noreply.github.com>
2018-09-03 22:30:06 +0530

    Add files via upload
    https://leetcode.com/problems/reverse-string/description/

---
+++ Leetcode/reverse-string.py
@@ -0,0 +1,11 @@
+__author__ = "niketanrane"
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        rs = s[::-1]
+        return rs
+        
