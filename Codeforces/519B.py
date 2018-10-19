dfbdda0abaf52896c2a7ecd460548f91fcd4cfff
Niketan Santosh Rane <40956529+niketanrane@users.noreply.github.com>
2018-10-19 19:37:57 +0530

    Add files via upload

---
+++ Codeforces/519B.py
@@ -0,0 +1,10 @@
+def main():
+    n = int(input())
+    A = sum(map(int, input().split()))
+    B = sum(map(int, input().split()))
+    C = sum(map(int, input().split()))
+
+    print(A-B)
+    print(B-C)
+
+main()
